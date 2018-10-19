bc15eec811703351dc36fd95126c3f83d011be3f
Ivo Georgiev <ivogeorg@gmail.com>
2018-10-18 20:52:45 -0600

    Initial commit

---
+++ exceptions.py
@@ -0,0 +1,10 @@
+inputs = [1, 56, 'street', 5.601, True]
+
+for j in range(len(inputs)):
+    i = 'undefined'
+    try:
+        i = int(inputs[j])
+    except ValueError as err:
+        print('Parse error: {0}'.format(err))
+    else:
+        print('Integer: {0}'.format(i))
