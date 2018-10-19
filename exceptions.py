c700a716c5ef1ef4826a1bb915df0462144e25db
Ivo Georgiev <ivogeorg@gmail.com>
2018-10-18 20:56:35 -0600

    i in scope outside try-except-else statement

--- exceptions.py
+++ exceptions.py
@@ -1,10 +1,12 @@
 inputs = [1, 56, 'street', 5.601, True]
 
 for j in range(len(inputs)):
-    i = 'undefined'
     try:
         i = int(inputs[j])
     except ValueError as err:
         print('Parse error: {0}'.format(err))
     else:
         print('Integer: {0}'.format(i))
+
+# i is still in scope here
+print(i)
