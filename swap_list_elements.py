e64d8c3f4bb1d742b5ec85b9beaffc6dd8451314
Ivo Georgiev <ivogeorg@gmail.com>
2018-10-22 12:01:05 -0600

    swap(l, i1, i2) for lists

---
+++ swap_list_elements.py
@@ -0,0 +1,21 @@
+import random
+
+
+def swap(l, i1, i2):
+    """assumes i1 < i2"""
+    first = l.pop(i1)
+    second = l.pop(i2-1)
+    l.insert(i1, second)
+    l.insert(i2, first)
+
+
+l = [random.randint(0, 1000) for _ in range(200)]
+s = [1, 5, 3, 0, 7]
+print(s)
+swap(s, 0, len(s)-1)
+print(s)
+swap(s, 2, 3)
+print(s)
+swap(s, 1, 3)
+print(s)
+
