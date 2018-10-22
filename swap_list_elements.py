fa137b60f488890ab78298cc6e68425b5c9015d7
Ivo Georgiev <ivogeorg@gmail.com>
2018-10-22 12:03:42 -0600

    removed index order assumption

--- swap_list_elements.py
+++ swap_list_elements.py
@@ -2,7 +2,10 @@ import random
 
 
 def swap(l, i1, i2):
-    """assumes i1 < i2"""
+    tmp = i1
+    if i1 > i2:
+        i1 = i2
+        i2 = tmp
     first = l.pop(i1)
     second = l.pop(i2-1)
     l.insert(i1, second)
@@ -18,4 +21,5 @@ swap(s, 2, 3)
 print(s)
 swap(s, 1, 3)
 print(s)
-
+swap(s, len(s)-1, 2)
+print(s)
