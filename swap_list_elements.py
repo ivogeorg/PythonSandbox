462f9464500eab914ec9685fe03c738ff9dec830
Ivo Georgiev <ivogeorg@gmail.com>
2018-10-22 12:29:12 -0600

    Added the pythonic way to swap, which uses ROT_TWO, and is much more efficient

--- swap_list_elements.py
+++ swap_list_elements.py
@@ -1,4 +1,5 @@
 import random
+import dis
 
 
 def swap(l, i1, i2):
@@ -12,6 +13,10 @@ def swap(l, i1, i2):
     l.insert(i2, first)
 
 
+def py_swap(l, i1, i2):
+    l[i1], l[i2] = l[i2], l[i1]
+
+
 l = [random.randint(0, 1000) for _ in range(200)]
 s = [1, 5, 3, 0, 7]
 print(s)
@@ -23,3 +28,15 @@ swap(s, 1, 3)
 print(s)
 swap(s, len(s)-1, 2)
 print(s)
+print()
+print(s)
+py_swap(s, 3, 1)
+print(s)
+py_swap(s, 0, 4)
+print(s)
+
+print()
+print('swap():')
+dis.dis(swap)
+print('py_swap():')
+dis.dis(py_swap)
