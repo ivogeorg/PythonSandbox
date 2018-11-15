026751cb95f84077a3d0770e466927ba023c4f42
Ivo Georgiev <ivogeorg@gmail.com>
2018-11-14 19:19:00 -0700

    Simple class with init options

---
+++ classes.py
@@ -0,0 +1,18 @@
+circle = 'circle'
+
+
+class CoinflipGame:
+    def __init__(self, di, sd, sk, sq):
+        self.diamond = di
+        self.small_diamond = sd
+        self.skull = sk
+        self.square = sq
+        self.circle = circle
+        self.ellipse = 'ellipse'
+
+    def display(self):
+        print(self.diamond, self.small_diamond, self.skull, self.square)
+
+
+game = CoinflipGame('diamond', 'small_diamond', 'skull', 'square')
+game.display()
